1d6abdeb23a8e2d61da02892d77dab7e56ae930a
tai928 <taisei.study928@gmail.com>
2026-03-28 17:24:46 +0900

    Update songs.js

--- songs.js
+++ songs.js
@@ -10,8 +10,7 @@ const songs = [
       { start: 9.8, end: 11.0, text: "ほら" },
       { start: 11.2, end: 14.7, text: "見失わないように" },
       { start: 15.3, end: 21.5, text: "手を離さないで", effect: "chorus",
-       overlays: [{ type: "sparkle", at: 4.00,duration: 2000,count: 20,spread: 220,x: 50,y: 46}]   
-        overlays: [{ type: "flash", at: 4.50,duration: 2000,count: 20,spread: 220,x: 50,y: 46}]},
+       overlays: [{ type: "sparkle", at: 4.00,duration: 2000,count: 20,spread: 220,x: 50,y: 46}],
       { start: 38.6, end: 42.2, text: "ねぇ耳を澄ませて" },
       { start: 42.9, end: 47.6, text: "星の降る音が聞こえるでしょう？" },
       { start: 47.0, end: 51.2, text: "もっと近くに来て" },
@@ -28,7 +27,8 @@ const songs = [
       { start: 69.5, end: 70.0, text: "la-" , effect: "pop" },
       { start: 70.0, end: 71.7, text: "la-!!" , effect: "pop" },
       { start: 71.2, end: 73.6, text: "暗がりの道も" },
-      { start: 73.6, end: 78.6, text: "月明かりが照らすの" },
+      { start: 73.6, end: 78.6, text: "月明かりが照らすの",
+       overlays: [{ type: "flash", at: 4.50,duration: 2000,count: 20,spread: 220,x: 50,y: 46}]}},
     ]
   },
   {
